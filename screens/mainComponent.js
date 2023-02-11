04cabf73a5dd065c5ccc77c0a622784d77b32a45
Ahmad Ayoub <ahmad.mmayoub@gmail.com>
2023-02-11 13:05:56 -0500

    New changes

--- screens/mainComponent.js
+++ screens/mainComponent.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { View } from "react-native";
 import { SONGS } from "../shared/songs";
 import DirectoryScreen from "./DirectoryScreen";
+import SongInfoScreen from "./SongInfoScreen";
 
 const Main = () => {
   const [songs, setSongs] = useState(SONGS);
+  const [songID, setSongID] = useState();
 
-  return <DirectoryScreen songs={songs} />;
+  return (
+    <View style={{ flex: 1 }}>
+      <DirectoryScreen songs={songs} onPress={(songID) => setSongID(songID)} />;
+      <SongInfoScreen song={songs.filter((song) => song.id === songID)[0]} />
+    </View>
+  );
 };
 
 export default Main;
