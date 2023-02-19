22700c1e44d4388ca6441952eed70e6e3dfaa209
Ahmad Ayoub <ahmad.mmayoub@gmail.com>
2023-02-19 17:02:24 -0500

    added notes explaining how the HomeScreenw works

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,15 @@ import { PARTNERS } from "../shared/partners";
 import { ScrollView } from "react-native-gesture-handler";
 import { useState } from "react";
 
+/*
+The FeaturedItem function takes in an object as a parameter which is a general list of information that each image would have. 
+Those would include the asset, text, the featured boolean and ID.
+This function would include an if statement as a spot check (SC) to make sure that the right info was selected.
+If not it will return an empty view. 
+
+When it works it will create a simple Card utilizing the image, name and description along with some minor CSS 
+*/
+
 const FeaturedItem = ({ item }) => {
   if (item) {
     return (
@@ -23,6 +32,22 @@ const FeaturedItem = ({ item }) => {
   }
   return <View />;
 };
+
+/*
+The HomeScreen function is tasked to list all the assets needed by destructuring the pages via the useState hook
+After that the singular featured asset found in each component is needed which would have the 'featured' prop of True
+
+Create a const variable assinged to the state variable 'songs' etc. it then uses the find method used usually to search
+for a boolean or singular item with the 'item' param since its also assigned to the FeaturedItem function and will be utilized there.
+But since 'item' is a general fill in the blank it needs another step to find the right asset. 
+
+So an arrow function is needed where within item we use dot notation to select the asset that has featured as true. 
+
+Lastly, return the FeaturedItem function with the item assigned to each feat(x) function since three are needed. 
+All three will be wrapped around the ScrollView tag which will allow the user to scroll through them on the app.
+
+
+*/
 
 const HomeScreen = () => {
   const [songs, setSongs] = useState(SONGS);
